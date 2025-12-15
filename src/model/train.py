1e614852d1328d752bd4c5401a05edc2dae439c9
Zwiswa Muridili <142395130+JepStar990@users.noreply.github.com>
2025-12-15 19:15:31 +0200

    Update train.py

--- src/model/train.py
+++ src/model/train.py
@@ -142,8 +142,11 @@ def train_one(variable: str, horizon: int):
         
         from datetime import datetime
         model_name = f"model_{datetime.now().strftime('%Y%m%d_%H%M%S')}"
-        ml_sklearn.log_model(model, name="model")
-        mlflow.register_model(f"runs:/{run_id}/model", model_name)
+        mlflow.sklearn.log_model(
+            sk_model=model,
+            artifact_path="model",  # for correct artifact folder
+            registered_model_name=model_name
+        )
         logger.info("Trained %s H+%d: RMSE=%.3f MAE=%.3f (run_id=%s)", variable, horizon, rmse, mae, run_id)
         
         # Register the run in DB (per variable & horizon)
